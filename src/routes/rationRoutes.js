9746bbbc7be43e5816f91757b659d67281674379
igo4ek <wawka2002@gmail.com>
2024-06-26 13:34:48 +0300

    Начало работы с рационами

--- src/routes/rationRoutes.js
+++ src/routes/rationRoutes.js
@@ -3,18 +3,18 @@
 const { Router } = require('express');
 const {
   createRation,
-  // getRations,
-  // getRationById,
-  // updateRationById,
-  // deleteRationById,
+  getRations,
+  getRationById,
+  updateRationById,
+  deleteRationById,
 } = require('../controllers/rationController');
 
 const router = Router();
 
 router.post('/', createRation);
-// router.get('/', getRations);
-// router.get('/:id', getRationById);
-// router.put('/:id', updateRationById);
-// router.delete('/:id', deleteRationById);
+router.get('/', getRations);
+router.get('/:id', getRationById);
+router.put('/:id', updateRationById);
+router.delete('/:id', deleteRationById);
 
 module.exports = router;
